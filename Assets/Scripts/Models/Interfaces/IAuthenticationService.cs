ef1d3240c4b794c3278633fa1bc4ac6dd8b1f481
Greg Quinn <greg@meltdowninteractive.com>
2025-07-31 12:20:41 +1200

    Added summaries to the interfaces

--- Assets/Scripts/Models/Interfaces/IAuthenticationService.cs
+++ Assets/Scripts/Models/Interfaces/IAuthenticationService.cs
@@ -4,8 +4,16 @@ namespace Models.Interfaces
 {
     public interface IAuthenticationService
     {
+        /// <summary>
+        /// Authenticates the user
+        /// </summary>
+        /// <param name="callback"></param>
         void Authenticate(Action<bool, string> callback);
         
+        /// <summary>
+        /// Logs the user out
+        /// </summary>
+        /// <param name="callback"></param>
         void Logout(Action callback);
     }
 }
--- Assets/Scripts/Models/Interfaces/IPlayerPrefsRepository.cs
+++ Assets/Scripts/Models/Interfaces/IPlayerPrefsRepository.cs
@@ -2,16 +2,69 @@
 {
     public interface IPlayerPrefsRepository
     {
+        /// <summary>
+        /// They player prefs key for the user access token
+        /// </summary>
         public string UserAccessTokenKey { get; }
         
+        /// <summary>
+        /// Checks if a key exists
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         bool KeyExists(string key);
+        
+        /// <summary>
+        /// Gets the string value of a key 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         string GetString(string key);
+        
+        /// <summary>
+        /// Gets the int value of a key
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         int GetInt(string key);
+        
+        /// <summary>
+        /// Gets the float value of a key
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         float GetFloat(string key);
+        
+        /// <summary>
+        /// Writes a string value to a key
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
         void WriteString(string key, string value);
+        
+        /// <summary>
+        /// Writes an int value to a key
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
         void WriteInt(string key, int value);
+        
+        /// <summary>
+        /// Writes a float value to a key
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
         void WriteFloat(string key, float value);
+        
+        /// <summary>
+        /// Deletes a key
+        /// </summary>
+        /// <param name="key"></param>
         void Delete(string key);
+        
+        /// <summary>
+        /// Clears all keys
+        /// </summary>
         void Clear();
     }
 }
